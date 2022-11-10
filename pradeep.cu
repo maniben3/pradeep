a2894d90fb1b4b342f7b9d461052f1ecf9e56e89
maniben3 <106887188+maniben3@users.noreply.github.com>
2022-11-11 01:23:40 +0530

    Update pradeep.cu

--- pradeep.cu
+++ pradeep.cu
@@ -1,13 +1,34 @@
-#include <iostream>
+#include <cuda.h>
+#include <cuda_runtime_api.h>
 
-using namespace std;
+__global__ void cuda_sum_kernel(float *a, float *b, float *c, size_t size)
+{
+    size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx >= size) {
+        return;
+    }
+
+    c[idx] = a[idx] + b[idx];
+}
 
 extern "C" {
-  __host__ int other(int n)
-  {
-          for(int i=0; i<n; i++)
-                  cout << "Hello, world!" << endl;
+void cuda_sum(float *a, float *b, float *c, size_t size)
+{
+    float *d_a, *d_b, *d_c;
 
-          return 0;
-  }
+    cudaMalloc((void **)&d_a, size * sizeof(float));
+    cudaMalloc((void **)&d_b, size * sizeof(float));
+    cudaMalloc((void **)&d_c, size * sizeof(float));
+
+    cudaMemcpy(d_a, a, size * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, b, size * sizeof(float), cudaMemcpyHostToDevice);
+
+    cuda_sum_kernel <<< ceil(size / 256.0), 256 >>> (d_a, d_b, d_c, size);
+
+    cudaMemcpy(c, d_c, size * sizeof(float), cudaMemcpyDeviceToHost);
+
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
+}
 }
