29e857ab88159ddbd78e4bd95c339fd45937af23
maniben3 <106887188+maniben3@users.noreply.github.com>
2022-11-12 09:33:29 +0530

    Update pradeep.cu

--- pradeep.cu
+++ pradeep.cu
@@ -1,13 +1,20 @@
 #include <iostream>
 
-using namespace std;
+using namespace std ;
 
-extern "C" {
-  __host__ int other(int n)
-  {
-          for(int i=0; i<n; i++)
-                  cout << "Hello, world!" << endl;
+# define DELLEXPORT extern "C" __declspec(dllexport)
 
-          return 0;
-  }
+__global__ void kernel(long* answer = 0){
+    *answer = threadIdx.x + (blockIdx.x * blockDim.x);
+}
+
+DELLEXPORT void resoult(long* h_answer){
+
+    long* d_answer = 0;
+    
+    cudaMalloc(&d_answer, sizeof(long));
+
+    kernel<<<10,1000>>>(d_answer);
+    cudaMemcpy(&h_answer, d_answer, sizeof(long), cudaMemcpyDeviceToHost);
+        cudaFree(d_answer);
 }
